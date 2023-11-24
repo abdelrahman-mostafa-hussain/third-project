1ec5b3beb2f100acff737e0f65c4a1ff71c22b88
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-24 23:35:28 +0200

    include function for operation

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,14 +1,26 @@
+def operation()->None:
+    print("This is the operation of this calculator:")
+    print("1- addition arithmetic")
+    print("2- subtraction arothmetic")
+    print("3- multiplication arithmetic")
+    print("4- division arithmetic")
+    print("5- quit the calculator")
+
 def addition(num1,num2)->float:
     return num1+num2
+
 def subtraction(num1,num2)->float:
     return num1-num2
+
 def multiplication(num1,num2)->float:
     return num1*num2
+
 def division(num1,num2)->float:
     return num1/num2
 
 def main():
     print("basic calculator")
+    #print("Enter the number of operation arithmetic you want: ")
 
 if __name__=="__main__":
     main()
