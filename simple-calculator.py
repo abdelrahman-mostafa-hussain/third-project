4b2ae199fb98b5523442d730dff7646f8884c0a1
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-24 23:24:31 +0200

    include the basic arithmetic  function

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,3 +1,12 @@
+def addition(num1,num2)->float:
+    return num1+num2
+def subtraction(num1,num2)->float:
+    return num1-num2
+def multiplication(num1,num2)->float:
+    return num1*num2
+def division(num1,num2)->float:
+    return num1/num2
+
 def main():
     print("basic calculator")
 
